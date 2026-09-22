a3e1cec6fc24a7693f8efd7498c06b19309e917b
windrunnerspk-stack <windrunnerspk@gmail.com>
2026-09-22 18:42:18 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vegas50k-v1';
+const CACHE = 'vegas50k-v2';
 const FILES = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
